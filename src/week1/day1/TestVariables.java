75a5a87756630555319c860f9373f150c9eaf09d
Artem Makogon <artemmakogon@continuuity.com>
2015-03-22 18:11:41 +0200

    initial commit

---
+++ src/week1/day1/TestVariables.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class TestVariables {
+  public static void main(String[] args) {
+    byte a = 5;
+    a = 10;
+    int b = 15;
+    int c = a;
+
+    double d = 5.7;
+
+    char qwe = 'c';
+
+    boolean bool = false;
+
+    System.out.println(a);
+    System.out.println(bool);
+
+
+
+
+
+  }
+}
